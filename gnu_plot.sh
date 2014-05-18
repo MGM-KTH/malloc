1ee8329af672a43bf5fc0fe90b84674edf6cb3ab
mrunelov <martin.runelov@gmail.com>
2014-05-18 16:27:20 +0200

    nicer plot

--- gnu_plot.sh
+++ gnu_plot.sh
@@ -2,5 +2,7 @@
 set terminal png
 set output "plot.png"
 set style data lines
-plot "first_fit.dat", "best_fit.dat"
+set style line 1 linecolor rgb '#0060ad'  linetype 1 linewidth 2
+set style line 2 linecolor rgb '#dd181f' linetype 1 linewidth 2
+plot "first_fit.dat" linestyle 1 smooth bezier, "best_fit.dat" linestyle 2 smooth bezier
 show output
