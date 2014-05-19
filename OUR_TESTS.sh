11e2e80a4bcd72e0054e9450845137490e501ada
mrunelov <martin.runelov@gmail.com>
2014-05-19 12:22:56 +0200

    pg flag on

--- OUR_TESTS.sh
+++ OUR_TESTS.sh
@@ -1,14 +1,16 @@
 gcc -c -w -O4 -DSTRATEGY=2 malloc.c
-gcc -c -w -O4 malloc_test_small.c
+gcc -c -w -O4 -pg malloc_test_small.c
 gcc -o malloc_test *.o
 echo "RUNNING BEST FIT..."
 ./malloc_test < test_data.in 2> best_fit.dat
+#/usr/bin/time ./malloc_test < test_data.in
 
 gcc -c -w -O4 -DSTRATEGY=1 malloc.c
-gcc -c -w -O4 malloc_test_small.c
+gcc -c -w -O4 -pg malloc_test_small.c
 gcc -o malloc_test *.o
 echo "RUNNING FIRST FIT..."
 ./malloc_test < test_data.in 2> first_fit.dat
+#/usr/bin/time ./malloc_test < test_data.in
 
 ./gnu_plot.sh
 if [ "$(uname)" == "Darwin" ]; then
